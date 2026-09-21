97440441a15fc0a669ce6ef42a78f4de8b9808ab
penguin <justapenguin974@gmail.com>
2026-09-21 15:21:35 -0400

    (nvim) Remove ' from autopairs

--- .config/nvim/lua/penguin/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/penguin/plugins/nvim-autopairs.lua
@@ -3,5 +3,10 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = true,
+	config = function()
+		local autopairs = require("nvim-autopairs")
+		autopairs.setup()
+
+		autopairs.remove_rule("'")
+	end,
 }
